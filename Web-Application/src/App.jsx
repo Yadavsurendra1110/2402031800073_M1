d01ee97e8948910d3e2e978868622c8f5a764647
Surendra <surendraspy2006@gmail.com>
2026-05-20 17:00:54 +0530

    created the website using the nodeand react library files and pushed the changes

---
+++ Web-Application/src/App.jsx
@@ -0,0 +1,167 @@
+import Head from "./Navigation/Head"
+
+function App()
+{
+    return(
+        <>
+        <Head/>
+        </>
+    )
+}
+
+export default App
+// import { useState } from 'react'
+// // import reactLogo from './assets/react.svg'
+// // import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import GtaVImg from './assets/GtaV.jpg'
+// import Reddead from './assets/Reddead.jpg'
+// import GtaVI from './assets/GtaVI.jp)g'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//             <header>
+//         <nav class="navbar">
+
+//             {/* <!-- Logo --> */}
+//             <div class="logo">
+//                 <h1>R★</h1>
+//             </div>
+
+//             {/* <!-- Navigation Menu --> */}
+//             <ul class="nav-links">
+//                 <li><a href="#">Games</a></li>
+//                 <li><a href="#">Newswire</a></li>
+//                 <li><a href="#">Videos</a></li>
+//                 <li><a href="#">Downloads</a></li>
+//                 <li><a href="#">Store</a></li>
+//                 <li><a href="#">Support</a></li>
+//             </ul>
+
+//              {/* Right Side Icons --> */}
+//             <div class="nav-icons">
+//                 <button class="launcher-btn">Get Launcher</button>
+//                 <i class="fa-solid fa-magnifying-glass"></i>
+//                 <i class="fa-solid fa-bars"></i>
+//             </div>
+
+//         </nav>
+//     </header>
+
+
+//     {/* HERO SECTION  */}
+
+//     <section class="hero">
+        
+//          {/* Dark Overlay */}
+//         <div class="overlay"></div>
+
+//          {/* Main Content  */}
+//         <div class="hero-content">
+
+//              {/* Left Side Logo  */}
+//             <div class="game-logo">
+//                  {/* <h2>grand<br>theft<br>auto <span>V</span></h2> */}
+//             </div>
+
+//              {/* Right Side Text  */}
+//             <div class="hero-text">
+//                  {/* <p><b>GRAND THEFT AUTO V</b></p> */}
+
+//                 <h1>
+//                     Experience GTAV Enhanced:<br/>
+//                     The Best Version on PC
+//                 </h1>
+
+//                  {/* Buttons */}
+//                 <div class="buttons">
+//                     <button class="watch-btn">
+//                         <i class="fa-solid fa-play"></i>
+//                         Watch Trailer
+//                     </button>
+
+//                     <button class="learn-btn">
+//                         Learn More
+//                     </button>
+                    
+//                 </div>
+//             </div>
+
+//         </div>
+
+//         {/* Slider Dots  */}
+//         <div class="slider">
+//             <span class="dot active"></span>
+//             <span class="dot"></span>
+//             <span class="dot"></span>
+//             <button id="pauseBtn">Pause</button>
+//         </div>
+
+//     </section>
+    
+
+//      {/* GAME CARDS   */}
+
+//     <section class="games-section">
+//       <div className='gridcont'>
+//         <img src={'https://media-rockstargames-com.akamaized.net/tina-uploads/posts/ak3ak31a49a221/c8c033070a0bfb2e3ec6ae7ff047ebe8ec551326.jpg?im=Resize,width=1200'} alt="GTA " />
+
+//       </div>
+
+//       <div className='cont'>
+//         <div className='arrangement'>
+//           <h2 className='arrangement'>Grand Theft Auto VI</h2><br/>
+//           <h1 className='arran'> Grand Theft Auto VI is Now Set to Launch November 19,2026</h1>
+
+//         </div>
+        
+//       </div>
+//         <h1 class="section-title">Featured Games</h1>
+
+//         <div class="cards-container">
+
+//             <div class="game-card">
+//                 <img src={GtaVImg} alt="GTA V"/>
+//                 <div class="card-content">
+//                     <h2>Grand Theft Auto V</h2>
+//                     <p>Explore Los Santos and complete missions.</p>
+//                 </div>
+              
+
+              
+//             </div>
+
+//             <div class="game-card">
+//                 <img src={Reddead} alt="RDR2"/>
+//                 <div class="card-content">
+//                     <h2>Red Dead Redemption 2</h2>
+//                     <p>Experience the wild west adventure.</p>
+//                 </div>
+//             </div>
+
+//             <div class="game-card">
+//                 <img src={GtaVI} alt="GTA VI"/>
+//                 <div class="card-content">
+//                     <h2>Grand Theft Auto VI</h2>
+//                     <p>Combination of best Open world game with the biggest map.</p>
+//                 </div>
+//             </div>
+
+//         </div>
+
+//     </section>
+
+
+
+//     <footer>
+//         <p>Rockstar Games | Created By Surendra Pratap Yadav</p>
+//     </footer>
+//     </>
+//   )
+// }
+
+// export default App
